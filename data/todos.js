3c030b75bdf212cf62ba7fdf3995f6e6e2c2aca3
Joseph Khan <jsphkhan@gmail.com>
2018-10-02 23:41:33 +0530

    initial commit

---
+++ data/todos.js
@@ -0,0 +1,15 @@
+{
+	"data" : [{
+		"title":"Bring Bread",
+		"completed": false
+	}, {
+		"title":"Pay Bills",
+		"completed": false
+	}, {
+		"title":"Car servicing",
+		"completed": false
+	}, {
+		"title":"Take wife out",
+		"completed": false
+	}]
+}
